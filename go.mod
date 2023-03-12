8c8e21b42db4e855f8b8a62b1839ccf9004595bd
Tornike Goshadze <tornikegoshadze@protonmail.com>
2023-03-12 23:38:39 +0400

    feat: try resolving n-sig in order to avoid throttling

--- go.mod
+++ go.mod
@@ -3,14 +3,19 @@ module github.com/joffarex/ytptk-go
 go 1.20
 
 require (
+	github.com/u2takey/ffmpeg-go v0.4.1
+	github.com/vbauerster/mpb/v8 v8.3.0
 	golang.org/x/net v0.8.0
 	golang.org/x/oauth2 v0.6.0
 	google.golang.org/api v0.112.0
+	rogchap.com/v8go v0.8.0
 )
 
 require (
 	cloud.google.com/go/compute v1.18.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
 	github.com/aws/aws-sdk-go v1.44.219 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
@@ -18,7 +23,8 @@ require (
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
 	github.com/googleapis/gax-go/v2 v2.7.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/u2takey/ffmpeg-go v0.4.1 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
--- ytpapi/videodl.go
+++ ytpapi/videodl.go
@@ -3,17 +3,22 @@ package ytpapi
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"github.com/joffarex/ytptk-go/sliceutils"
-	ffmpeg_go "github.com/u2takey/ffmpeg-go"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"github.com/vbauerster/mpb/v8"
+	"github.com/vbauerster/mpb/v8/decor"
 	"google.golang.org/api/youtube/v3"
 	"io"
 	"log"
 	"net/http"
+	neturl "net/url"
 	"os"
 	"os/user"
 	"path/filepath"
+	jsinterp "rogchap.com/v8go"
 	"strings"
+	"sync"
+	"time"
 )
 
 type InputClientMainApp struct {
@@ -59,21 +64,68 @@ type Output struct {
 	StreamingData StreamingData `json:"StreamingData"`
 }
 
-// WriteCounter counts the number of bytes written to it.
-type WriteCounter struct {
-	Total   int64 // Total # bytes to be transferred
-	Current int64 // Current # amount of bytes transferred
-}
+func handleScopeAsync(wg *sync.WaitGroup, progress *mpb.Progress, jsinterpContext *jsinterp.Context, url string, path string, scope string) *ffmpeg.Stream {
+	wg.Add(1)
 
-// Write implements the io.Writer interface.
-//
-// Always completes and never returns an error.
-func (wc *WriteCounter) Write(p []byte) (int, error) {
-	n := len(p)
-	wc.Current += int64(n)
-	fmt.Printf("Chunk size: %d | Current Sum: %d | Total: %d | Left: %d\n", n, wc.Current, wc.Total, wc.Total-wc.Current)
-	//fmt.Printf("Read %d bytes for a total of %d\n", n, wc.Current)
-	return n, nil
+	barName := "[" + scope + "]"
+	bar := progress.AddBar(
+		0,
+		mpb.PrependDecorators(
+			decor.Name(barName, decor.WC{W: len(barName) + 1, C: decor.DidentRight}),
+			decor.CountersKiloByte("% .2f / % .2f"),
+		),
+		mpb.AppendDecorators(
+			decor.Percentage(decor.WCSyncSpace),
+			decor.Name(" | ETA: "),
+			decor.OnComplete(decor.EwmaETA(decor.ET_STYLE_GO, 60, decor.WCSyncWidth), "done"),
+			decor.Name(" | Speed: "),
+			decor.EwmaSpeed(decor.UnitKB, "% .2f", 60),
+		),
+	)
+
+	go func() {
+		client := &http.Client{}
+
+		parsedUrl, _ := neturl.Parse(url)
+		params := parsedUrl.Query()
+
+		jsinterpContext.RunScript("var iha=function(_){var n=_.split(\"\"),e=[1085621920,-1413232877,245246939,-214478372,-1947691414,-1440840439,function(_,n){n=(n%_.length+_.length)%_.length;var e=_[0];_[0]=_[n],_[n]=e},-28067232,function(_,n){for(n=(n%_.length+_.length)%_.length;n--;)_.unshift(_.pop())},-1253364940,1108783393,1993082094,-2141271272,-1236697005,2066803531,null,-122169779,1673105371,952868508,1462692941,function(){for(var _=64,n=[];++_-n.length-32;){switch(_){case 58:_-=14;case 91:case 92:case 93:continue;case 123:_=47;case 94:case 95:case 96:continue;case 46:_=95}n.push(String.fromCharCode(_))}return n},1737827501,2066803531,99253448,function(_,n){n=(n%_.length+_.length)%_.length,_.splice(0,1,_.splice(n,1,_[0])[0])},661884814,n,1987089113,1657437625,1981674291,function(_){for(var n=_.length;n;)_.push(_.splice(--n,1)[0])},\"Y8eupT\",-188239353,-811835968,n,-795230947,null,1894196126,function(_,n){n=(n%_.length+_.length)%_.length,_.splice(-n).reverse().forEach(function(n){_.unshift(n)})},n,-259629667,function(_,n,e){var t=e.length;_.forEach(function(_,n,$){this.push($[n]=e[(e.indexOf(_)-e.indexOf(this[n])+n+t--)%e.length])},n.split(\"\"))},245246939,null,-1804836242,421896565,function(_,n){n=(n%_.length+_.length)%_.length,_.splice(n,1)},1108783393,function(_){_.reverse()},-1755683472,function(_,n){_.push(n)}];e[15]=e,e[36]=e,e[43]=e;try{e[24](e[36],e[23]),e[38](e[43],e[44]),e[12](e[30],e[9]),e[42](e[43],e[8]),e[3](e[40],e[2]),e[34](e[47]),e[50](e[32]),e[28](e[43],e[21]),e[28](e[38],e[37]),e[12](e[30],e[44]),e[50](e[38],e[39]),e[10](e[19],e[11]),e[45](e[30],e[35],e[24]()),e[42](e[47],e[5]),e[25](e[13],e[34]),e[47](e[26],e[1]),e[47](e[2],e[12]),e[51](e[36],e[35]),e[1](e[8],e[25]),e[24](e[42],e[19]),e[22](e[0],e[28]),e[24](e[3],e[27]),e[12](e[3],e[45]),e[10](e[50],e[26]),e[12](e[16],e[43]),e[12](e[50],e[0]),e[2](e[31],e[29]),e[9](e[13]),e[25](e[5],e[50]),e[27](e[31],e[49]),e[25](e[13],e[20]),e[37](e[5],e[40]),e[30](e[31],e[32]),e[28](e[46]),e[13](e[30],e[19]),e[37](e[1],e[7]),e[25](e[6],e[45]),e[13](e[39],e[5])}catch(t){return\"enhanced_except_z5QBsOv-_w8_\"+_}return n.join(\"\")};", "n-resolver.js")
+		nResolved, _ := jsinterpContext.RunScript("iha("+params.Get("n")+")", "main.js")
+
+		params.Del("n")
+		params.Add("n", nResolved.String())
+
+		parsedUrl.RawQuery = params.Encode()
+
+		request, requestError := http.NewRequest(http.MethodGet, parsedUrl.String(), nil)
+		if requestError != nil {
+			log.Fatalf("Request %s error: %v", scope, requestError.Error())
+		}
+		// ----------------------
+		download, downloadErr := client.Do(request)
+		if downloadErr != nil {
+			log.Fatalf("Download %s error: %v", scope, downloadErr.Error())
+		}
+
+		file, _ := os.Create(filepath.Join(path, scope+".mp4"))
+
+		bar.SetTotal(download.ContentLength, false)
+		var src io.Reader
+		src = download.Body
+		proxyReader := bar.ProxyReader(src)
+		defer proxyReader.Close()
+		_, audioErr := io.Copy(file, proxyReader)
+
+		if audioErr != nil {
+			log.Fatalf("%s error: %v", scope, audioErr.Error())
+		}
+
+		defer wg.Done()
+	}()
+
+	inputAudio := ffmpeg.Input(filepath.Join(path, scope+".mp4"))
+
+	return inputAudio
 }
 
 func DownloadVideoToDestination(service *youtube.Service, videoId string, destination string) {
@@ -100,67 +152,32 @@ func DownloadVideoToDestination(service *youtube.Service, videoId string, destin
 	var output Output
 	_ = json.Unmarshal(bytes, &output)
 
-	audio := sliceutils.Filter(output.StreamingData.AdaptiveFormats, func(item AdaptiveFormat) bool {
-		return strings.Contains(item.MimeType, "audio/mp4") && item.AudioQuality == "AUDIO_QUALITY_MEDIUM"
-	})[0]
-	downloadAudio, downloadAudioErr := http.Get(audio.Url)
-	if downloadAudioErr != nil {
-		log.Fatalf("Download Audio error: %v", downloadAudioErr.Error())
-	}
-
-	fullHD := sliceutils.Filter(output.StreamingData.AdaptiveFormats, func(item AdaptiveFormat) bool {
-		return strings.Contains(item.MimeType, "video/mp4") && item.Height == 1080 && item.Quality == "hd1080"
-	})[0]
-	downloadFullHd, downloadFullHdErr := http.Get(fullHD.Url)
-	if downloadFullHdErr != nil {
-		log.Fatalf("Download Audio error: %v", downloadFullHdErr.Error())
-	}
-
 	osUser, _ := user.Current()
 	path := filepath.Join(osUser.HomeDir, "ytptk", "_temp", destination)
 	os.MkdirAll(path, 0700)
-	audioFile, _ := os.Create(filepath.Join(path, "audio.mp4"))
-	fullHDFile, _ := os.Create(filepath.Join(path, "fullHD.mp4"))
 
-	var audioSrc io.Reader
-	audioSrc = downloadAudio.Body
-	_, audioErr := io.Copy(audioFile, audioSrc)
+	// --------------- URLs ---------------
+	audio := sliceutils.Filter(output.StreamingData.AdaptiveFormats, func(item AdaptiveFormat) bool {
+		return strings.Contains(item.MimeType, "audio/mp4") && item.AudioQuality == "AUDIO_QUALITY_MEDIUM"
+	})[0]
+	video := sliceutils.Filter(output.StreamingData.AdaptiveFormats, func(item AdaptiveFormat) bool {
+		return strings.Contains(item.MimeType, "video/mp4") && item.Quality == "hd1080"
+	})[0]
 
-	if audioErr != nil {
-		log.Fatalf("Audio error: %v", audioErr.Error())
-	}
+	var wg sync.WaitGroup
 
-	var fullHDSrc io.Reader
-	fullHDSrc = downloadFullHd.Body
-	_, fullHDErr := io.Copy(fullHDFile, fullHDSrc)
+	progress := mpb.New(mpb.WithWidth(64), mpb.WithWaitGroup(&wg), mpb.WithRefreshRate(180*time.Millisecond))
 
-	if fullHDErr != nil {
-		log.Fatalf("Full HD error: %v", fullHDErr.Error())
-	}
+	ctx := jsinterp.NewContext()
 
-	inputAudio := ffmpeg_go.Input(filepath.Join(path, "audio.mp4"))
-	inputFullHD := ffmpeg_go.Input(filepath.Join(path, "fullHD.mp4"))
-	encodingErr := ffmpeg_go.Concat([]*ffmpeg_go.Stream{inputAudio, inputFullHD}, ffmpeg_go.KwArgs{"v": 1, "a": 1}).Output(filepath.Join(osUser.HomeDir, "ytptk", destination+".mp4")).Run()
+	ffmpegInputAudio := handleScopeAsync(&wg, progress, ctx, audio.Url, path, "audio")
+	ffmpegInputVideo := handleScopeAsync(&wg, progress, ctx, video.Url, path, "video")
+
+	wg.Wait()
+
+	encodingErr := ffmpeg.Concat([]*ffmpeg.Stream{ffmpegInputAudio, ffmpegInputVideo}, ffmpeg.KwArgs{"v": 1, "a": 1}).Output(filepath.Join(osUser.HomeDir, "ytptk", destination+".mp4")).Run()
 
 	if encodingErr != nil {
 		log.Fatalf("Encoding error: %v", encodingErr.Error())
 	}
-
-	// -------------------------------------------------
-	//download, _ := http.Get(output.StreamingData.Formats[0].Url)
-	//
-	//osUser, _ := user.Current()
-	//path := filepath.Join(osUser.HomeDir, "ytptk")
-	//os.MkdirAll(path, 0700)
-	//outFile, _ := os.Create(filepath.Join(path, destination+".mp4"))
-	//
-	//var src io.Reader
-	//src = download.Body
-	//
-	//videosResponse, error := service.Videos.List([]string{"fileDetails"}).Id(videoId).Do()
-	//fmt.Printf("\nVideo: %v\n", error)
-	//
-	//src = io.TeeReader(src, &WriteCounter{Total: int64(videosResponse.Items[0].FileDetails.FileSize)})
-	//
-	//_, _ = io.Copy(outFile, src)
 }
